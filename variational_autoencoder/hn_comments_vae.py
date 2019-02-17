1d98d0d04014e2892d7c2ec74c8e11e74868f069
Matt Ritter <matthew.w.ritter@gmail.com>
2019-02-16 20:03:21 -0500

    Got hn_comments_vae.py working!

--- variational_autoencoder/hn_comments_vae.py
+++ variational_autoencoder/hn_comments_vae.py
@@ -26,19 +26,16 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from keras.layers import Dense, Input
-from keras.layers import Conv2D, Flatten, Lambda
-from keras.layers import Reshape, Conv2DTranspose
+from keras.layers import Dense, Input, Embedding
+from keras.layers import Flatten, Lambda
 from keras.models import Model
-from keras.datasets import mnist
+from keras.initializers import Constant
 from keras.losses import mse, binary_crossentropy
 from keras.utils import plot_model
 from keras import backend as K
 
-import numpy as np
-import matplotlib.pyplot as plt
 import argparse
-import os
+import h5py
 
 
 # reparameterization trick
@@ -61,163 +58,102 @@ def sampling(args):
     epsilon = K.random_normal(shape=(batch, dim))
     return z_mean + K.exp(0.5 * z_log_var) * epsilon
 
+###
+# Sanity checks to prevent freezing
+###
+K.clear_session()
 
-def plot_results(models,
-                 data,
-                 batch_size=128,
-                 model_name="vae_mnist"):
-    """Plots labels and MNIST digits as function of 2-dim latent vector
+# IF IT DOES NOT WORK, MAY NEED TO RESTART COMPUTER
 
-    # Arguments:
-        models (tuple): encoder and decoder models
-        data (tuple): test data and label
-        batch_size (int): prediction batch size
-        model_name (string): which model is using this function
-    """
+# confirm TensorFlow sees the GPU
+from tensorflow.python.client import device_lib
+assert 'GPU' in str(device_lib.list_local_devices())
 
-    encoder, decoder = models
-    x_test, y_test = data
-    os.makedirs(model_name, exist_ok=True)
+# confirm Keras sees the GPU
+from keras import backend
+assert len(backend.tensorflow_backend._get_available_gpus()) > 0
 
-    filename = os.path.join(model_name, "vae_mean.png")
-    # display a 2D plot of the digit classes in the latent space
-    z_mean, _, _ = encoder.predict(x_test,
-                                   batch_size=batch_size)
-    plt.figure(figsize=(12, 10))
-    plt.scatter(z_mean[:, 0], z_mean[:, 1], c=y_test)
-    plt.colorbar()
-    plt.xlabel("z[0]")
-    plt.ylabel("z[1]")
-    plt.savefig(filename)
-    plt.show()
+# confirm PyTorch sees the GPU
+from torch import cuda
+assert cuda.is_available()
+assert cuda.device_count() > 0
+print(cuda.get_device_name(cuda.current_device()))
 
-    filename = os.path.join(model_name, "digits_over_latent.png")
-    # display a 30x30 2D manifold of digits
-    n = 30
-    digit_size = 28
-    figure = np.zeros((digit_size * n, digit_size * n))
-    # linearly spaced coordinates corresponding to the 2D plot
-    # of digit classes in the latent space
-    grid_x = np.linspace(-4, 4, n)
-    grid_y = np.linspace(-4, 4, n)[::-1]
+def main():
+    with h5py.File('data/indexed.h5', 'r') as f_indexed:
+        x_train = f_indexed['training_data'][()]
+        sequence_length = x_train.shape[1]
 
-    for i, yi in enumerate(grid_y):
-        for j, xi in enumerate(grid_x):
-            z_sample = np.array([[xi, yi]])
-            x_decoded = decoder.predict(z_sample)
-            digit = x_decoded[0].reshape(digit_size, digit_size)
-            figure[i * digit_size: (i + 1) * digit_size,
-                   j * digit_size: (j + 1) * digit_size] = digit
+    with h5py.File('data/w2v_weights_and_word_index_mapping.h5', 'r') as f_w2v:
+        embedding_matrix = f_w2v['weights'][()]
+        num_distinct_words, embedding_dim = embedding_matrix.shape
 
-    plt.figure(figsize=(10, 10))
-    start_range = digit_size // 2
-    end_range = n * digit_size + start_range + 1
-    pixel_range = np.arange(start_range, end_range, digit_size)
-    sample_range_x = np.round(grid_x, 1)
-    sample_range_y = np.round(grid_y, 1)
-    plt.xticks(pixel_range, sample_range_x)
-    plt.yticks(pixel_range, sample_range_y)
-    plt.xlabel("z[0]")
-    plt.ylabel("z[1]")
-    plt.imshow(figure, cmap='Greys_r')
-    plt.savefig(filename)
-    plt.show()
+    # network parameters
+    input_shape = (sequence_length, )
+    latent_dim = 2  # The number of dimensions to project onto
+    batch_size = 32  # TODO Update once working
+    epochs = 2  # TODO Update once working
 
 
-# MNIST dataset
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+    # VAE model = encoder + decoder
+    # build encoder model
+    inputs = Input(shape=input_shape, name='encoder_input')
 
-sequence_length = 300 # From data/indexed.h5
-w2v_dims = 100  # From data/w2v_weights_and_word_index_mapping.h5
-x_train = np.reshape(x_train, [-1, image_size, image_size, 1])
-x_test = np.reshape(x_test, [-1, image_size, image_size, 1])
-x_train = x_train.astype('float32') / 255
-x_test = x_test.astype('float32') / 255
+    # This is the most efficient way to include the w2v
+    # Doing the joins outside of the GPU is impractical
+    embedded_sequences = Embedding(num_distinct_words,
+                                embedding_dim,
+                                embeddings_initializer=Constant(embedding_matrix),
+                                input_length=sequence_length,
+                                trainable=False)(inputs)
 
-# network parameters
-input_shape = (300, image_size, 1)
-batch_size = 128
-kernel_size = 3
-filters = 16
-latent_dim = 2
-epochs = 30
+    # I don't want to deal with CNN right now
+    # I'm not sure if it's possible, since it would be outside of the
+    # optimization loop. I guess you could use Conv1DTranspose
+    post_cnn = embedded_sequences
 
-# VAE model = encoder + decoder
-# build encoder model
-inputs = Input(shape=input_shape, name='encoder_input')
-x = inputs
-for i in range(2):
-    filters *= 2
-    x = Conv2D(filters=filters,
-               kernel_size=kernel_size,
-               activation='relu',
-               strides=2,
-               padding='same')(x)
+    # generate latent vector Q(z|X)
+    flattened = Flatten(name='flattened')(post_cnn)
+    dense1 = Dense(16, activation='relu', name='dense1')(flattened)
+    z_mean = Dense(latent_dim, name='z_mean')(dense1)
+    z_log_var = Dense(latent_dim, name='z_log_var')(dense1)
 
-# shape info needed to build decoder model
-shape = K.int_shape(x)
+    # use reparameterization trickshape to push the sampling out as input
+    # note that "output_shape" isn't necessary with the TensorFlow backend
+    z = Lambda(sampling, output_shape=(latent_dim,), name='z')([z_mean, z_log_var])
 
-# generate latent vector Q(z|X)
-x = Flatten()(x)
-x = Dense(16, activation='relu')(x)
-z_mean = Dense(latent_dim, name='z_mean')(x)
-z_log_var = Dense(latent_dim, name='z_log_var')(x)
+    # instantiate encoder model
+    encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder')
+    encoder.summary()
+    plot_model(encoder, to_file='vae_cnn_encoder.png', show_shapes=True)
 
-# use reparameterization trick to push the sampling out as input
-# note that "output_shape" isn't necessary with the TensorFlow backend
-z = Lambda(sampling, output_shape=(latent_dim,), name='z')([z_mean, z_log_var])
+    # build decoder model
+    latent_inputs = Input(shape=(latent_dim,), name='z_sampling')
+    outputs = Dense(K.int_shape(flattened)[1], activation='sigmoid')(latent_inputs)  # TODO probably add more layers
 
-# instantiate encoder model
-encoder = Model(inputs, [z_mean, z_log_var, z], name='encoder')
-encoder.summary()
-plot_model(encoder, to_file='vae_cnn_encoder.png', show_shapes=True)
+    # instantiate decoder model
+    decoder = Model(latent_inputs, outputs, name='decoder')
+    decoder.summary()
+    plot_model(decoder, to_file='vae_cnn_decoder.png', show_shapes=True)
 
-# build decoder model
-latent_inputs = Input(shape=(latent_dim,), name='z_sampling')
-x = Dense(shape[1] * shape[2] * shape[3], activation='relu')(latent_inputs)
-x = Reshape((shape[1], shape[2], shape[3]))(x)
+    # instantiate VAE model
+    outputs = decoder(encoder(inputs)[2])
+    vae = Model(inputs, outputs, name='vae')
 
-for i in range(2):
-    x = Conv2DTranspose(filters=filters,
-                        kernel_size=kernel_size,
-                        activation='relu',
-                        strides=2,
-                        padding='same')(x)
-    filters //= 2
-
-outputs = Conv2DTranspose(filters=1,
-                          kernel_size=kernel_size,
-                          activation='sigmoid',
-                          padding='same',
-                          name='decoder_output')(x)
-
-# instantiate decoder model
-decoder = Model(latent_inputs, outputs, name='decoder')
-decoder.summary()
-plot_model(decoder, to_file='vae_cnn_decoder.png', show_shapes=True)
-
-# instantiate VAE model
-outputs = decoder(encoder(inputs)[2])
-vae = Model(inputs, outputs, name='vae')
-
-if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     help_ = "Load h5 model trained weights"
     parser.add_argument("-w", "--weights", help=help_)
     help_ = "Use mse loss instead of binary cross entropy (default)"
     parser.add_argument("-m", "--mse", help=help_, action='store_true')
     args = parser.parse_args()
-    models = (encoder, decoder)
-    data = (x_test, y_test)
 
     # VAE loss = mse_loss or xent_loss + kl_loss
     if args.mse:
-        reconstruction_loss = mse(K.flatten(inputs), K.flatten(outputs))
+        reconstruction_loss = mse(K.flatten(flattened), K.flatten(outputs))
     else:
-        reconstruction_loss = binary_crossentropy(K.flatten(inputs),
+        reconstruction_loss = binary_crossentropy(K.flatten(flattened),
                                                   K.flatten(outputs))
 
-    reconstruction_loss *= image_size * image_size
     kl_loss = 1 + z_log_var - K.square(z_mean) - K.exp(z_log_var)
     kl_loss = K.sum(kl_loss, axis=-1)
     kl_loss *= -0.5
@@ -227,6 +163,11 @@ if __name__ == '__main__':
     vae.summary()
     plot_model(vae, to_file='vae_cnn.png', show_shapes=True)
 
+    from keras.callbacks import TensorBoard as tb
+    from datetime import datetime
+    t = datetime.now()
+    tensorboard = tb(log_dir='/home/mritter/code/twitter_nlp/full_hn_usernames/tensorboard_logs/{:%Y-%m-%d-%H-%M}'.format(t))
+
     if args.weights:
         vae = vae.load_weights(args.weights)
     else:
@@ -234,7 +175,9 @@ if __name__ == '__main__':
         vae.fit(x_train,
                 epochs=epochs,
                 batch_size=batch_size,
-                validation_data=(x_test, None))
+                callbacks=[tensorboard])
         vae.save_weights('vae_cnn_mnist.h5')
 
-    plot_results(models, data, batch_size=batch_size, model_name="vae_cnn")
+
+if __name__ == '__main__':
+    main()
